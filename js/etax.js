c5d7ef9450c4eb81ae7460e7723b3e6740304639
ayazibrahimov <ayaz.ibrahimov.1998@mail.ru>
2022-07-14 10:18:25 +0400

    aaa

--- js/etax.js
+++
@@ -1,6 +0,0 @@
-function taxCreate() {
-    document.querySelector(".taxPageOne").style.display = "none";
-    document.querySelector(".taxPageTwo").style.display = "block";
-    document.querySelector(".drop__box-etax--header").textContent="Invocies"
-    }
-  
